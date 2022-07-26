7146afad28b2b759288e508141c1e23cfa4d2d8c
Noah Gearhart <excelsiorvfx@gmail.com>
2022-07-25 21:41:07 -0400

    Initial Commit

---
+++ Hubs/DefaultHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace blitz.Hubs
+{
+    public class DefaultHub : Hub
+    {
+        public async Task Hello()
+        {
+            await Clients.All.SendAsync("Hello");
+        }
+    }
+}
